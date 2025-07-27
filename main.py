6020d01862f6fbab0c8ed4eb6edac8ee83ee18ff
Showzun <echozun@gmail.com>
2025-07-27 00:32:28 -0500

    Final changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, count_chars, sort_chars
+import sys
 
 # takes a filepath and returns the contents of the file as a string
 def get_book_text(filepath):
@@ -7,15 +8,19 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
-    string_book = get_book_text("books/frankenstein.txt")
-    word_count = count_words(string_book)
-    print(f"Found {word_count} total words\n--------- Character Count -------")
-    chars_count = count_chars(string_book)
-    sorted_chars = sort_chars(chars_count)
-    for char in sorted_chars:
-        if char["char"].isalpha():
-            print(f"{char["char"]}: {char["num"]}")
-    print("============= END ===============")
+    if len(sys.argv) < 2:
+        raise Exception("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
+        string_book = get_book_text(sys.argv[1])
+        word_count = count_words(string_book)
+        print(f"Found {word_count} total words\n--------- Character Count -------")
+        chars_count = count_chars(string_book)
+        sorted_chars = sort_chars(chars_count)
+        for char in sorted_chars:
+            if char["char"].isalpha():
+                print(f"{char["char"]}: {char["num"]}")
+        print("============= END ===============")
 
 main()
